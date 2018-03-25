eee5a9b64b45393815272fc795e419e02d786361
Stephan Köninger <github@stekoe.de>
2018-03-25 17:30:11 +0200

    format code

--- src/main/java/de/stekoe/springbootangular/SpringBootAngularApplication.java
+++ src/main/java/de/stekoe/springbootangular/SpringBootAngularApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootAngularApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAngularApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAngularApplication.class, args);
+    }
 }
